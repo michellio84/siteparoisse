0ef936ba18508d4b5c6a75c8095a2a4ea7f6e1fb
Michel Lio <michellio@MacBook-Air-de-Michel.local>
2024-07-14 00:13:47 +0200

    Création de la page d'accueil avec le nouveau design

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const jours = [
-        "dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"
-    ];
-
-    const horaires = {
-        "lundi": "Messe à 9h précédée des Laudes à 8h30.",
-        "mardi": "Messe à 9h précédée des Laudes à 8h30.",
-        "mercredi": "Laudes à 8h30 suivies de la messe (9h00) et de l’adoration du St Sacrement jusqu’à 10h30. Réconciliation après la messe.",
-        "jeudi": "Messe à 9h précédée des Laudes à 8h30.",
-        "vendredi": "Laudes à 8h30 suivies de la messe (9h00) et de l’adoration du St Sacrement jusqu’à 10h30. Réconciliation après la messe.",
-        "samedi": "Laudes à 8h30, Messe à 18h.",
-        "dimanche": "Messe des familles à 10h30."
-    };
-
-    const today = new Date();
-    const dayName = jours[today.getDay()];
-    const horairesDuJour = document.getElementById("horaires-du-jour");
-
-    if (horaires[dayName]) {
-        horairesDuJour.textContent = horaires[dayName];
-    } else {
-        horairesDuJour.textContent = "Aucun horaire disponible pour aujourd'hui.";
-    }
+document.addEventListener('DOMContentLoaded', (event) => {
+    const services = document.querySelectorAll('.service');
+    services.forEach((service, index) => {
+        setTimeout(() => {
+            service.classList.add('fade-in');
+        }, index * 200);
+    });
 });
