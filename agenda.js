1998c5d3ac272f4bf54e6f29debab20b1117a743
Michel Lio <michellio@MacBook-Air-de-Michel.local>
2024-07-20 01:03:31 +0200

    adaptation visualisation agenda

--- agenda.js
+++ agenda.js
@@ -13,37 +13,21 @@ document.addEventListener('DOMContentLoaded', function() {
                 const eventElement = document.createElement('div');
                 eventElement.className = 'event-item';
                 eventElement.innerHTML = `
-                    <div class="image-container">
-                        <img src="${event.image}" alt="${event.title}" >
-                    </div>    
-                    <div class="event-content">
-                        <h2>${event.title}</h2>
-                        <div class="event-info">
-                            <i class="fas fa-calendar-alt event-icon"></i>
-                            <span>${new Date(event.date).toLocaleDateString()}</span>
-                        </div>
-                        <div class="event-info">
-                            <i class="fas fa-map-marker-alt event-icon"></i>
-                            <span>${event.location}</span>
-                        </div>
-                        <p class="event-description">${event.description}</p>
-                        <a href="${event.link}" class="event-link">En savoir plus</a>
+                    <img src="${event.image}" alt="${event.title}">
+                    <h2>${event.title}</h2>
+                    <div class="event-info">
+                        <i class="fas fa-calendar-alt event-icon"></i>
+                        <span>${new Date(event.date).toLocaleDateString()}</span>
                     </div>
+                    <div class="event-info">
+                        <i class="fas fa-map-marker-alt event-icon"></i>
+                        <span>${event.location}</span>
+                    </div>
+                    <p class="event-description">${event.description}</p>
+                    <a href="${event.link}" class="event-link">En savoir plus</a>
                 `;
                 container.appendChild(eventElement);
-                // Fetch the markdown content for each article
-                fetch(`/content/agenda/${event.slug}.md`)
-                    .then(response => {
-                        console.log(`Fetch markdown response for ${event.slug}:`, response);
-                        return response.text();
-                    })
-                    .then(markdown => {
-                        console.log(`Markdown content for ${event.slug}:`, markdown);
-                        const contentElement = document.createElement('div');
-                        contentElement.innerHTML = marked(markdown); // Utilisation d'un parseur Markdown
-                        eventElement.appendChild(contentElement);
-                    })
-                    .catch(error => console.error(`Error loading content for ${event.slug}:`, error));
             });
-        });
+        })
+        .catch(error => console.error('Error loading events:', error));
 });
