109d550e17ab3f3ab875b76f2b3744af3910feb8
Michel Lio <michellio@MacBook-Air-de-Michel.local>
2024-07-20 00:04:31 +0200

    debug test 2 agenda

--- scripts/generate-agenda.js
+++ scripts/generate-agenda.js
@@ -2,30 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const matter = require('gray-matter');
 
-const eventsDirectory = path.join(process.cwd(), 'content/agenda');
-const outputDirectory = path.join(process.cwd(), 'public');
-const outputFilePath = path.join(outputDirectory, 'agenda-index.json');
+const contentDir = path.join(process.cwd(), 'content', 'agenda');
+const outputFile = path.join(contentDir, 'agenda-index.json');
 
-function generateAgendaIndex() {
-    const fileNames = fs.readdirSync(eventsDirectory);
-    const events = fileNames.map(fileName => {
-        const filePath = path.join(eventsDirectory, fileName);
-        const fileContents = fs.readFileSync(filePath, 'utf8');
-        const { data, content } = matter(fileContents);
-        
-        return {
-            ...data,
-            content
-        };
-    });
+const events = fs.readdirSync(contentDir)
+  .filter(filename => filename.endsWith('.md'))
+  .map(filename => {
+    const filePath = path.join(contentDir, filename);
+    const content = fs.readFileSync(filePath, 'utf8');
+    const { data } = matter(content);
+    return {
+      title: data.title,
+      date: data.date,
+      slug: filename.slice(0, -3), // Remove .md extension
+      image: data.image,
+      description: data.description
+    };
+})
+  .sort((a, b) => new Date(b.date) - new Date(a.date));
 
-    // Créez le répertoire 'public' s'il n'existe pas
-    if (!fs.existsSync(outputDirectory)) {
-        fs.mkdirSync(outputDirectory);
-    }
-
-    fs.writeFileSync(outputFilePath, JSON.stringify(events, null, 2));
-    console.log('agenda-index.json generated successfully');
-}
-
-generateAgendaIndex();
+fs.writeFileSync(outputFile, JSON.stringify(events, null, 2));
+console.log('index.json generated successfully');
