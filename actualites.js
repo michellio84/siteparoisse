2a40cc847888fe736569b1c46665d63e0d675750
michellio84 <michellio84@gmail.com>
2024-10-28 23:46:02 +0100

    changement titre actualité

--- actualites.js
+++ actualites.js
@@ -1,21 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM fully loaded and parsed');
+    
     fetch('/content/actualites/index.json')
-        .then(response => response.json())
+        .then(response => {
+            console.log('Fetch response:', response);
+            return response.json();
+        })
         .then(data => {
+            console.log('Fetched data:', data);
             const container = document.getElementById('news-container');
             data.forEach(article => {
                 const articleElement = document.createElement('div');
                 articleElement.className = 'news-item';
                 
-                // Structure pour afficher la liste avec un lien cliquable sur le titre pour accéder au contenu complet
+                // Ajoute une icône avant le titre
                 articleElement.innerHTML = `
-                    <h2><a href="article.html?slug=${article.slug}">${article.title}</a></h2>
+                    <h2><i class="fas fa-file-alt"></i> <a href="article.html?slug=${article.slug}">${article.title}</a></h2>
                     <p class="date">Publié le ${new Date(article.date).toLocaleDateString()}</p>
                     <p>${article.description}</p>
                 `;
-                
                 container.appendChild(articleElement);
             });
         })
-        .catch(error => console.error('Erreur lors du chargement des articles :', error));
+        .catch(error => console.error('Error loading articles:', error));
 });
