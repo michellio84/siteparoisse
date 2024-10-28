20000cf8cb15d31092f62ed37616a4f959f200d7
michellio84 <michellio84@gmail.com>
2024-10-28 15:13:39 +0100

    Ajout de la page article.html et mise à jour de actualites.js pour liens dynamiques

--- actualites.js
+++ actualites.js
@@ -1,26 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM fully loaded and parsed');
-    
     fetch('/content/actualites/index.json')
-        .then(response => {
-            console.log('Fetch response:', response);
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
-            console.log('Fetched data:', data);
             const container = document.getElementById('news-container');
             data.forEach(article => {
                 const articleElement = document.createElement('div');
                 articleElement.className = 'news-item';
                 
-                // Structure simplifiée pour afficher uniquement la liste avec titre, date, et description
+                // Structure pour afficher la liste avec un lien cliquable sur le titre pour accéder au contenu complet
                 articleElement.innerHTML = `
-                    <h2><a href="/actualite/${article.slug}.html">${article.title}</a></h2>
+                    <h2><a href="article.html?slug=${article.slug}">${article.title}</a></h2>
                     <p class="date">Publié le ${new Date(article.date).toLocaleDateString()}</p>
                     <p>${article.description}</p>
                 `;
+                
                 container.appendChild(articleElement);
             });
         })
-        .catch(error => console.error('Error loading articles:', error));
+        .catch(error => console.error('Erreur lors du chargement des articles :', error));
 });
